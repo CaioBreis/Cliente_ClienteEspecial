e3ac8b9c6cba045e8cc9b8d5e2a42253373ffe23
Caio Bispo dos Reis <165906386+CaioBreis@users.noreply.github.com>
2024-04-23 02:23:21 -0300

    Update Main Banco.java

--- Main Banco.java
+++ Main Banco.java
@@ -1,52 +1,33 @@
 package teste;
 
-
-
-import enti.Cliente;
+import enti.ClienteComum;
 import enti.Cliente_Especial;
 
 import java.util.Scanner;
 
-
 public class Banco {
     public static void main(String[] args) {
-
-
         Scanner sc = new Scanner(System.in);
 
-        String nome;
-        int id;
-        double saldo;
-        int cont;
+        ClienteComum clientes[] = new ClienteComum[3];
+        clientes[0] = new ClienteComum("Asdrubal", 123, 200);
+        clientes[1] = new Cliente_Especial("Mariana", 456, 3000, 100);
+        clientes[2] = new ClienteComum("Juan", 789, 500);
 
-
-        Cliente d[] = new Cliente[3];
-        d[0]=new Cliente("Asdrubal",123, 200);
-        System.out.println(" ");
-        d[1]=new Cliente_Especial("Mariana",456, 3000,100);
-        System.out.println(" ");
-        d[2]=new Cliente("Juan",789, 500);
         System.out.println("-----------------------------");
-
-
-        for(int i=0; i<d.length; i++) {
-            System.out.println(d[i]);
+        for (ClienteComum cliente : clientes) {
+            System.out.println(cliente);
             System.out.println(" ");
         }
 
         System.out.println("-------------------------");
-
-        for(int i=0; i<d.length; i++) {
-            d[i].nalteraSalario(900);
-
+        for (ClienteComum cliente : clientes) {
+            cliente.nalteraSaldo(900);
         }
 
-        for(int i=0; i<d.length; i++) {
-            System.out.println(d[i]);
+        for (ClienteComum cliente : clientes) {
+            System.out.println(cliente);
             System.out.println(" ");
         }
-
     }
-
-
 }
