c21447cb7e24d1bd6696fef8603868e37dcbbd7e
Caio Bispo dos Reis <165906386+CaioBreis@users.noreply.github.com>
2024-04-23 02:22:29 -0300

    Update Obj ClienteEspecial.java

--- Obj ClienteEspecial.java
+++ Obj ClienteEspecial.java
@@ -1,27 +1,31 @@
 package enti;
 
-public class Cliente extends ClienteEspecial {
+public class Cliente_Especial extends ClienteComum {
     protected double limite;
 
-    public gerente(String nome, int id, double saldo, double limite) {
+    public Cliente_Especial(String nome, int id, double saldo, double limite) {
         super(nome, id, saldo);
         this.limite = limite;
-
     }
 
-    public double nalteraSalario(double vlr) {
+    @Override
+    public double nalteraSaldo(double vlr) {
         if (vlr <= this.saldo + this.limite) {
             this.saldo -= vlr;
             return saldo;
         } else {
-            System.out.println("nao tem o limite");
-            return limite+saldo-vlr;
+            System.out.println("Limite excedido.");
+            return saldo; // ou retorne um valor específico, dependendo do comportamento desejado
         }
-
     }
 
     @Override
     public String toString() {
-        return "Cliente Especial{" + "Nome=" + this.nome + " ,id=" + this.id + " ,saldo=" + this.saldo + " ,limite=" + this.limite + "}";
+        return "Cliente_Especial{" +
+                "nome='" + this.nome + '\'' +
+                ", id=" + id +
+                ", saldo=" + saldo +
+                ", limite=" + limite +
+                '}';
     }
 }
