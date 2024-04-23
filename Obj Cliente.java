9eef0248222843b91d0ddf1535c66551bf7d978f
Caio Bispo dos Reis <165906386+CaioBreis@users.noreply.github.com>
2024-04-23 02:04:11 -0300

    Create Obj Cliente.java

---
+++ Obj Cliente.java
@@ -0,0 +1,55 @@
+package enti;
+
+public class cliente {
+    protected String nome;
+    protected  int id;
+    protected  double saldo;
+
+    public funcionario(String nome, int id, double saldo) {
+        this.nome = nome;
+        this.id = id;
+        this.saldo = saldo;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getSalario() {
+        return saldo;
+    }
+
+    public void setSalario(double salario) {
+        this.saldo = salario;
+    }
+    public void alteraSalario(double deposito) {
+         this.saldo += deposito;
+
+    }
+    public double nalteraSalario(double saque) {
+       saldo= saldo - saque;
+        return this.saldo;
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "funcionario{" +
+                "nome='" + this.nome + '\'' +
+                ", id=" + id +
+                ", saldo=" + saldo +
+                '}';
+    }
+}
